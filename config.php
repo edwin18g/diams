745c8ef21567fe387750f44e01cb9ead2dafb093
edwin <edwin2021.resbee@gmail.com>
2024-02-25 20:01:04 +0530

    d

--- config.php
+++ config.php
@@ -1,21 +1,24 @@
 <?php
 
 
-if($_SERVER['HTTP_HOST'] == 'dio.local'){
+if($_SERVER['HTTP_HOST'] == 'dio.local')
+{
 	define('HOSTNAME', 'localhost');
 	define('USERNAME', 'root');
 	define('PASSWORD', 'root');
 	define('DATABASE', 'dio');
-}elseif($_SERVER['HTTP_HOST'] == 'dio.local.com'){
+}elseif($_SERVER['HTTP_HOST'] == 'dio.local.com')
+{
 	define('HOSTNAME', 'localhost');
 	define('USERNAME', 'root');
 	define('PASSWORD', 'root');
 	define('DATABASE', 'dio');
-}else{
+}else
+{
 	define('HOSTNAME', 'localhost');
-		define('USERNAME', 'edwinpok_Stream');
-		define('PASSWORD', '12345678');
-		define('DATABASE', 'edwinpok_kuzhithuraidiocese');
+	define('USERNAME', 'edwinpok_Stream');
+	define('PASSWORD', '12345678');
+	define('DATABASE', 'edwinpok_kuzhithuraidiocese');
 }
 	// Database Connection
 
