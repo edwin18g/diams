6ac6efadfde44f39f792edea73eae18637d007c6
edwin <edwin2021.resbee@gmail.com>
2024-03-01 20:30:25 +0530

    d

--- codeigniter/application/config/database.php
+++ codeigniter/application/config/database.php
@@ -94,9 +94,7 @@ $db['default']		= array(
 	'failover' 		=> array(),
 	'save_queries' 	=> TRUE
 );
-echo "<pre>";
-print_r($db['default']);
-die;
+
 /*
 [hostname] => sql211.epizy.com
 [username] => epiz_21887212
