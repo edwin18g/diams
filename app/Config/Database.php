8cc13d78fe34d9a64ebd4d17a4f38c8ee9db3da2
Abiya <abiya.resbee@gmail.com>
2024-03-10 13:29:32 +0530

    0sdfsd

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => 'root',
+		'password' => 'Root@123',
 		'database' => 'dio_new',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '_',
